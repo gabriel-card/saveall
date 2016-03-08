34c6e465696035610848343889483277e8e2915a
Gabriel Cardoso <gabriel.cardoso@corp.globo.com>
2016-03-08 10:18:12 -0300

    cria modelo

--- models.py
+++ models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.nome
+
+
+class Raca(models.Model):
+    nome_raca = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.nome_raca
+
+
+class Animal(models.Model):
+    nome = models.CharField(max_length=30)
+    dono = models.ForeignKey(Pessoa)
+    raca = models.ForeignKey(Raca)
+
+    def __unicode__(self):
+        return self.nome
