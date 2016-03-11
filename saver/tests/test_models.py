d1d9bd6e5fca8f968214d495ef71ab63fb8326ef
Gabriel Cardoso <gabriel.cardoso@corp.globo.com>
2016-03-11 16:03:49 -0300

    renomeia models e testes para entendimento

--- saver/tests/test_models.py
+++ saver/tests/test_models.py
@@ -1,25 +1,25 @@
 from django.test import TestCase
 from django.utils.six import StringIO
 from django.core.management import call_command
-from saver.models import Pessoa, Animal, Raca
+from saver.models import Table01, Table02, Table03
 
 
 class ModelsIntegrityTest(TestCase):
 
     def setUp(self):
         self.out = StringIO()
-        p1 = Pessoa.objects.create(nome="Gabriel")
-        r1 = Raca.objects.create(nome_raca="Golden")
-        Animal.objects.create(nome="Arthas", dono=p1, raca=r1)
+        p1 = Table01.objects.create(nome="row01tb01")
+        r1 = Table02.objects.create(nome="row01tb02")
+        Table03.objects.create(nome="row01tb03", dono=p1, raca=r1)
 
     def test_integrity_saveall_command_create_update_datetime(self):
-        created = Pessoa.objects.filter(pk=1).values('created')[0]['created']
-        old_updated = Pessoa.objects.filter(pk=1).values('updated')[0]['updated']
+        created = Table01.objects.filter(pk=1).values('created')[0]['created']
+        old_updated = Table01.objects.filter(pk=1).values('updated')[0]['updated']
 
-        call_command('saveall', 'saver.Pessoa', stdout=self.out)
+        call_command('saveall', 'saver.Table01', stdout=self.out)
 
-        new_created = Pessoa.objects.filter(pk=1).values('created')[0]['created']
-        new_updated = Pessoa.objects.filter(pk=1).values('updated')[0]['updated']
+        new_created = Table01.objects.filter(pk=1).values('created')[0]['created']
+        new_updated = Table01.objects.filter(pk=1).values('updated')[0]['updated']
 
         self.assertNotEqual(old_updated, new_updated)
         self.assertEqual(created, new_created)
