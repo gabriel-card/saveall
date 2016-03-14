c5befb171dea59c7cde0e3ec469f031ce6cc3215
Gabriel Cardoso <gabriel.cardoso@corp.globo.com>
2016-03-14 13:39:46 -0300

    starts release 0.1.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-saveall',
-    version='0.1.3',
+    version='0.1.4',
     packages=['saveall'],
     include_package_data=True,
     license='MIT License',
